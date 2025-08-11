6d0338f7351035e088482f1e58917e6147b8ef39
Yanendrajha <Yanendrajha37@gmail.com>
2025-08-11 23:38:54 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,46 @@
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args){
-    if (args.length != 2 || !args[0].equals("-E")) {
-      System.out.println("Usage: ./your_program.sh -E <pattern>");
-      System.exit(1);
+    public static void main(String[] args) {
+        if (args.length != 2 || !args[0].equals("-E")) {
+            System.out.println("Usage: ./your_program.sh -E <pattern>");
+            System.exit(1);
+        }
+        
+        String pattern = args[1];
+        Scanner scanner = new Scanner(System.in);
+        String inputLine = scanner.nextLine();
+        
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.err.println("Logs from your program will appear here!");
+        
+        // Uncomment this block to pass the first stage
+        
+        if (matchPattern(inputLine, pattern)) {
+            System.exit(0);
+        } else {
+            System.exit(1);
+        }
     }
-
-    String pattern = args[1];  
-    Scanner scanner = new Scanner(System.in);
-    String inputLine = scanner.nextLine();
-
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-
-     if (matchPattern(inputLine, pattern)) {
-         System.exit(0);
-     } else {
-         System.exit(1);
-     }
-  }
-
-  public static boolean matchPattern(String inputLine, String pattern) {
-    if (pattern.length() == 1) {
-      return inputLine.contains(pattern);
-      } else if (pattern.contains("\\d")) {
-          return inputLine.chars().anyMatch(Character::isDigit);
-      } else {
-      throw new RuntimeException("Unhandled pattern: " + pattern);
+    
+    public static boolean matchPattern(String inputLine, String pattern) {
+        if (pattern.length() == 1) {
+            return inputLine.contains(pattern);
+        } else if (pattern.contains("\\d")) {
+            return inputLine.chars().anyMatch(Character::isDigit);
+        } else if (pattern.contains("\\w")) {
+            return checkIsAlphaNumeric(inputLine);
+        } else {
+            throw new RuntimeException("Unhandled pattern: " + pattern);
+        }
     }
-  }
+    
+    private static boolean checkIsAlphaNumeric(String inputLine) {
+        for (char c : inputLine.toCharArray()) {
+            if (Character.isLetterOrDigit(c) || c == '_') {
+                return true;
+            }
+        }
+        return false;
+    }
 }
